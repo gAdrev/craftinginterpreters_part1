cb48b6dff0423ae5e2db03b974e78427484e621d
Javier V <javiervilla00@gmail.com>
2018-02-04 21:30:55 +0100

    GenerateAst script (section 5.2)

---
+++ src/org/javi/compilers/jlox/Expr.java
@@ -0,0 +1,17 @@
+package org.javi.compilers.jlox;
+
+abstract class Expr {
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        private final Expr left;
+        private final Expr right;
+        private final Token operator;
+    }
+
+
+}
