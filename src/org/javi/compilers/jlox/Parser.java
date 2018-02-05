df86ab3685aa1089ae14207e83aedc3c65a34c31
Javier V <javiervilla00@gmail.com>
2018-02-06 00:41:33 +0100

    Parser up to comparison()

--- src/org/javi/compilers/jlox/Parser.java
+++ src/org/javi/compilers/jlox/Parser.java
@@ -1,5 +1,6 @@
 package org.javi.compilers.jlox;
 
+import static org.javi.compilers.jlox.TokenType.*;
 import java.util.List;
 
 public class Parser {
@@ -8,5 +9,66 @@ public class Parser {
 
     Parser(List<Token> tokens) {
         this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while(match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr comparison() {
+        Expr expr = addition();
+
+        while(match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = addition();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type: types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(TokenType tokenType) {
+        if (isAtEnd()) return false;
+        return peek().type == tokenType;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
     }
 }
